2228cea081b9bbd80a3051c9c27197b0b05e8760
tbdatasci <tbdatasci@gmail.com>
2020-12-14 11:40:07 -0500

    Trying out previously commented out Actor insertion

--- app.py
+++ app.py
@@ -62,21 +62,21 @@ def create_app(test_config=None):
         try:
             # This actor = ... line is the problem
             actor = Actor(name=new_name, age=new_age, gender=new_gender)
-            # actor.insert()
-            # new_actor = actor.format()
-
-            # return jsonify({
-            #     'success': True,
-            #     'actor': new_actor
-            # }), 200
+            actor.insert()
+            new_actor = actor.format()
 
             return jsonify({
                 'success': True,
-                'actor': "Your Mom",
-                'name': new_name,
-                'age': new_age,
-                'gender': new_gender
+                'actor': new_actor
             }), 200
+
+            # return jsonify({
+            #     'success': True,
+            #     'actor': "Your Mom",
+            #     'name': new_name,
+            #     'age': new_age,
+            #     'gender': new_gender
+            # }), 200
         except Exception as E:
             abort(422)
 
